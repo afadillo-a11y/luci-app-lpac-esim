86e1286765180817e939920eaf725bba721c5071
afadillo <afadillo@gmail.com>
2026-03-31 23:07:30 +0300

    Delete luci-app-epm directory

--- luci-app-epm/luasrc/model/cbi/epm.lua
+++
@@ -1,7 +0,0 @@
--- /usr/lib/lua/luci/model/cbi/epm.lua
-local map = Map("epm", translate("eSIM Profile Manager"), translate("Manage eSIM profiles using lpac"))
-
--- Usa un template custom per i tab
-map.template = "epm/main"
-
-return map
